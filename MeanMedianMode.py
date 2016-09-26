0d9723df643fe1ea24aa3237d9bd6225f6ea6e05
Nick <nick@hattwick.com>
2016-09-26 16:29:46 -0500

    Add existing file

--- MeanMedianMode.py
+++ MeanMedianMode.py
@@ -2,7 +2,6 @@ import numpy as np
 from scipy import stats
 
 def mmm(x):
-    return np.mean(x)
-    return np.median(x)
-    return stats.mode(x)
-    
+    return np.mean(x) #calculates mean
+    return np.median(x) #calculates median
+    return stats.mode(x) #calculates mode
