40b4e31df53b5ba4925ff109f2dce4352f9c59ce
Nick <nick@hattwick.com>
2016-10-01 22:30:54 -0500

    Add existing file

--- Dtest1.py
+++ Dtest1.py
@@ -1,3 +1,5 @@
+import random
+
 Deck = []
 def addlands(xli):
     x = 0
@@ -8,16 +10,32 @@ def addlands(xli):
 addlands(Deck)
 
 Deck.extend((2,2,2,2,2,2,3,3,3,3,3,3,4,4,4,4,5,5,6))
-print Deck
-print len(Deck)
 
 def Game():
     class player:
         life = 20
         deck = Deck
+        field = []
+        hand = []
+        graveyard = []
 
         def __init__(self, name):
             self.name = name
 
-P1 = player('Player 1')
-p2 = player('Player 2')
+
+    P1 = player('Player 1')
+    P2 = player('Player 2')
+
+
+    random.shuffle(P1.deck)
+    random.shuffle(P2.deck)
+
+    for i in 7:
+        x = P1.deck.pop()
+        P1.hand.extend(x)
+        y = P2.deck.pop()
+        P2.hand.extend(y)
+
+    print P1.hand
+
+Game
